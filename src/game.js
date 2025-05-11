e17ec5c87471b9cbae5fbd7c2e89586bb87a1096
Tomasz Styczeń <97025562+Tomek889@users.noreply.github.com>
2025-05-11 22:08:59 +0200

    Initialize startGame function and export it to index.js

---
+++ src/game.js
@@ -0,0 +1,7 @@
+import Player from './classes/Player';
+import Ship from './classes/Ship';
+
+export default function startGame() {
+  const user = new Player(true);
+  const computer = new Player(false);
+}
