092d43add7ebe30f7297c8b1c1af1469e9232da9
Tomasz Styczeń <97025562+Tomek889@users.noreply.github.com>
2025-05-12 17:56:42 +0200

    Implement shootUser that attacks a random cell

--- src/game.js
+++ src/game.js
@@ -1,11 +1,42 @@
 import Player from './classes/Player';
 import Ship from './classes/Ship';
-import { renderBoard, updateBoard } from './dom';
+import renderBoard from './dom';
+
+let user;
+let computer;
 
 export default function startGame() {
-  const user = new Player(true);
-  const computer = new Player(false);
+  user = new Player(true);
+  computer = new Player(false);
 
   renderBoard(user.board, 'player-board');
   renderBoard(computer.board, 'computer-board');
 }
+
+function shootUser() {
+  let randomX = Math.floor(Math.random() * 10);
+  let randomY = Math.floor(Math.random() * 10);
+
+  let isLeftCell = false;
+  for (let i = 0; i < 10; i += 1) {
+    for (let j = 0; j < 10; j += 1) {
+      if (user.board.board[j][i] === 0) {
+        isLeftCell = true;
+      }
+    }
+  }
+
+  if (!isLeftCell) {
+    return;
+  }
+
+  while (user.board.board[randomY][randomX] !== 0) {
+    randomX = Math.floor(Math.random() * 10);
+    randomY = Math.floor(Math.random() * 10);
+  }
+
+  user.board.receiveAttack(randomX, randomY);
+  renderBoard(user.board, 'player-board');
+}
+
+export { shootUser };
